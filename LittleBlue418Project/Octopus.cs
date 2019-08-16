224ce1a8d068fa328998f531a4a54da42e9697a1
LittleBlue418 <hollyathomas88@gmail.com>
2019-08-16 15:50:42 +0200

    created octopus class, created three octopus instances

---
+++ LittleBlue418Project/Octopus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LittleBlue418Project
+{
+    //Class
+    public class Octopus
+    {
+        // Attributes
+        int size;
+        string color;
+
+        // Constructor
+        public Octopus(int size, string color)
+        {
+            this.size = size;
+            this.color = color;
+        }
+
+        //Function
+        public void PrintDescription()
+        {
+            Console.WriteLine($"I am a {size}cm {color} octopus");
+        }
+
+    }
+}
---
+++ LittleBlue418Project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LittleBlue418Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create object | making an instance of it | set the instance | create a new one | of this class | input the missing variables from the object.
+            Octopus myOctopus1 = new Octopus(20, "purple");
+            // calling our instance | calling a function from within the class
+            myOctopus1.PrintDescription();
+
+            Octopus myOctopus2 = new Octopus(10, "yello");
+            myOctopus2.PrintDescription();
+
+            Octopus myOctopus3 = new Octopus(2, "black");
+            myOctopus3.PrintDescription();
+
+
+        }
+    }
+}
